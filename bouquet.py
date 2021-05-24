07761b7a387d324704b31bf9c71bddd5d020f1e8
gutobiogit <gutobioit@gmail.com>
2021-05-24 13:49:36 +0200

    Final v0.1a

--- bouquet.py
+++ bouquet.py
@@ -1,9 +1,33 @@
+import re
+
 class bouquet():
+
+    """
+    A class(input::string) used to represent an flower bouquet
+    input="<"A"-"Z"><"L","S"><<1-9><"a"-"z">>...<1-99>"
+    ...
+
+    Methods
+    -------
+    design_name()
+        Set name of the bouquet from string::buq_formula
+        Output: "A".."Z"
+
+    size()
+        Set size of the bouquet from string::buq_formula
+        Output: "S","L"
+    
+    max_size()
+        Set max size of the bouquet from string::buq_formula
+        Output: 1..9
+    
+    flowers()
+    Set flowers of the bouquet from string::buq_formula
+    Output: {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
+    """
 
     def __init__(self, formula_bouquet) -> str:
         self.formula_bouquet = formula_bouquet
-        self.summ=0
-        self.flowers_control={}
 
     def design_name(self):
         design_name = self.formula_bouquet[0]
@@ -21,67 +45,160 @@ class bouquet():
         return last_char
 
     def flowers(self):
-        flowers = {self.formula_bouquet[i+1]+self.size() :int(self.formula_bouquet[i]) for i in range(2, len(self.formula_bouquet)-2, 2)}
+        flowers = {}
+        size = self.size()
+        var = 0
+        for i in self.formula_bouquet[2:]:
+            if i.isalpha():
+                flowers[i+size]=int(var)
+                var = 0
+            else:
+                var+= int(i)
         return flowers
-    
-    def summatory(self,summatory="empty") -> int:
-        if summatory == 0:
-            self.summ=0
-        elif str(summatory).isnumeric():
-            self.summ+=summatory
-        return self.summ
 
-    def flowers_used(self, value= "empty") -> str:
-        if value == 0:
-            self.flowers_control.clear()
-        elif value == "empty":
-            return self.flowers_control
-        elif value in self.flowers_control:
-            self.flowers_control[value]+=1
+#Class create for coloring the terminal, should work on all ANSI/VT100 terminals
+class colors:
+    RED = '\033[31m'
+    ENDC = '\033[m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+
+#redu(adds::int)
+#Reduce the flower formula, check if max::int value is equal or smaller then the value needed adds::int, if so interate to fill final::dict
+def redu(adds):
+     if adds >= max:
+        sume= adds-max
+        for x in final:
+            for y in list(final)[1:]:
+                if final[x]>1 and all_flowers[y]-1 > 0 or x == y :
+                    final[x]-= 1
+                    sume-= 1
+            if sume == 0:
+                return
+        redu(adds-1) 
+
+#Prepare the pretty print with the last check, distributing the flowers to empty slots
+def check_complete():
+    pprint=''
+    look_fail=flower.flowers()
+    for  flow in look_fail:
+        if flow in final:
+            pass
         else:
-            self.flowers_control[value]=1
+            return ""
+    for flow in final:
+        if final[flow] == 0:
+            zero_place=flow
+            for alter in final:
+                if final[alter] > 2:
+                    final[alter]-= 1
+                    final[zero_place]+= 1
+    for flow in final:
+        pprint+=str(final[flow]) + flow[0]
+    for y in all_flowers:
+        if y in final:
+            all_flowers[y]-= final[y]    
+    return pprint
 
-def process():
+all_buq_formula = [] #create var all_buq_formula::all_flowers for all flowers buquet inputs
+all_flowers = {} #create var all_flower::dictionary to store all flowers in stock
 
-    for flower in all_buq_formula:
-        flower.summatory(0)
-        flower.flowers_used()
-        one_set=flower.flowers()
-        big_size= flower.max_size()
-        for dic_key in one_set:
-            if dic_key in all_flowers:
-                if all_flowers[dic_key] >= one_set[dic_key]:
-                    flower.summatory(int(one_set[dic_key]))
-                    flower.flowers_used(dic_key)
-                else:
-                    flower.summatory(int(all_flowers[dic_key]))
-                if flower.summatory() == int(big_size):
-                    flower_summary=flower.flowers_used()
-                    for end_list in flower_summary:
-                        all_flowers[end_list]-= flower_summary[end_list]
-                    flower.summatory(0)
-                    flower.flowers_used(0)
-
-
-    return
-
-all_buq_formula = [] #create global var all_buq_formula::list for all flowers buquet inputs
-all_flowers = {} #create global var all_flower::dictionary to store all flowers in stock
 
 #BOUQUET FORMULA INPUT
 while True:
-    buq_formula = input("Enter the bouquet formula: ") 
-    if buq_formula == "": #if inputI/O::string == ""::string, break the BUQUET FORMULA INPUT (if input is empty it has reach the end of the bouquet formula input,break the while)
+    buq_formula = input(f"{colors.BLUE}Enter the bouquet formula: {colors.ENDC}") 
+    if buq_formula == "" and len(all_buq_formula) > 0 : #if inputI/O::string == ""::string, break the BUQUET FORMULA INPUT (if input is empty it has reach the end of the bouquet formula input,break the while)
         break             
     else:
-        all_buq_formula.append(bouquet(buq_formula))#else append inputI/O::string to all_buq_formula::list (all_buq_formula is where all bouquet formulas are stored)
+        if re.match(r"\A[A-Z][L,S]([0-9]+[a-z])+[1-9]\d{0,1}\Z", buq_formula):
+            all_buq_formula.append(bouquet(buq_formula))#else append inputI/O::string to all_buq_formula::all_flowers (all_buq_formula is where all bouquet formulas are stored)
+        else:
+            print(f"{colors.RED}Wrong buquet formula input format !{colors.ENDC}")    
+
+
 #FLOWERS INPUT
 while True:
-    flower_formula = input("Enter flowers: ")
-    if flower_formula in all_flowers:
-        all_flowers[flower_formula] += 1
+    flower_formula = input(f"{colors.GREEN}Enter flowers: {colors.ENDC}")
+    if flower_formula == "" and len(all_flowers) > 0 : #if inputI/O::string == ""::string, break the BUQUET FORMULA INPUT (if input is empty it has reach the end of the bouquet formula input,break the while)
+        break
+    if re.match(r"[a-z][L,S]$",flower_formula):
+        if flower_formula in all_flowers:
+            all_flowers[flower_formula]+= 1
+        else:
+            all_flowers[flower_formula]= 1
     else:
-        all_flowers[flower_formula] = 1
-    process()
+        print(f"{colors.RED}Wrong flower input format !{colors.ENDC}") 
 
 
+#MAIN INTERATION WITH OBJECTS
+for flower in all_buq_formula * 10:
+    name = flower.design_name()#name::string = flower.design.name()::string
+    size_of = flower.size()#size_of ::string = flower.size()::string
+    one_set = flower.flowers()#one_set::dictionary = flower.flowers()::dictionary
+    max = int(flower.max_size())#max::int = flower.max_size()::string
+    adds = 0
+    final = {}#creating 
+    for buqu in one_set:
+        if buqu in all_flowers:
+            if all_flowers[buqu] >= 1:
+                final[buqu]=one_set[buqu]               
+    for summing in final:
+        adds+= final[summing]
+    redu(adds)
+    response=check_complete()
+    if response != "":
+        print(colors.BLUE,"BOUQUET FOUND ---> ", name,size_of,response, colors.ENDC,sep = "")
+
+# import unittest
+# from unittest import mock
+ 
+# class Test_bloomon(unittest.TestCase):
+ 
+
+    # @mock.patch(f'{__name__}1.foo', autospec=True, side_effect=bouquet("AS2a2b3"))
+    # def test_foo_return_and_local_params_values(self, mocked):
+        # self.assertEqual('A', mocked.side_effect.design_name) #PASS
+        # self.assertEqual("S", mocked.side_effect.size) #PASS
+        # self.assertEqual(3, mocked.side_effect.max_size) #PASS 
+        # self.assertEqual({'a':2,'b':2}, mocked.side_effect.flowers) #PASS
+
+    # @mock.patch(f'{__name__}2.foo', autospec=True, side_effect=bouquet("BL2a2"))
+    # def test_foo_return_and_local_params_values(self, mocked):
+        # self.assertEqual('B', mocked.side_effect.design_name) #PASS
+        # self.assertEqual("L", mocked.side_effect.size) #PASS
+        # self.assertEqual(2, mocked.side_effect.max_size) #PASS 
+        # self.assertEqual({'a':2}, mocked.side_effect.flowers) #PASS
+
+    # @mock.patch(f'{__name__}3.foo', autospec=True, side_effect=bouquet("CL2a2b3f4g5q2"))
+    # def test_foo_return_and_local_params_values(self, mocked):
+        # self.assertEqual('C', mocked.side_effect.design_name) #PASS
+        # self.assertEqual("L", mocked.side_effect.size) #PASS
+        # self.assertEqual(2, mocked.side_effect.max_size) #PASS 
+        # self.assertEqual({'a':2,'b':3,'f':3,'g':4,'q':5}, mocked.side_effect.flowers) #PASS
+
+
+    # def TEST_check_complete(self):
+        #type(self).max = 3
+        #type(self).final={'as':2,'bs':2}
+        # self.assertEqual(redu(4),type(self).final={'as':1,'bs':2}) #PASS
+
+        #type(self).max = 2
+        #type(self).final={'as':2,'bs':2}
+        # self.assertEqual(redu(4),type(self).final={'as':1,'bs':1}) #PASS
+
+        #type(self).max = 3
+        #type(self).final={'as':2,'bs':2}
+        # self.assertEqual(redu(3),type(self).final={'as':1,'bs':1}) #PASS
+
+        #type(self).max = 4
+        #type(self).final={'as':2,'bs':2}
+        # self.assertEqual(redu(3),type(self).final={'as':2,'bs':2}) #PASS
+
+        #type(self).max = 5
+        #type(self).final={'as':2,'bs':2}
+        # self.assertEqual(redu(5),type(self).final={}) #PASS
+        
+        #type(self).max = 1
+        #type(self).final={'as':1}
+        # self.assertEqual(redu(5),type(self).final={'as':1,'bs':1}) #PASS
